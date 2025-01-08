95e886a2d0d1c5213455936609951fc6d29a8893
shahedul-alam <shaheedalamcontact@gmail.com>
2025-01-08 13:02:19 +0600

    get api for all jobs added

--- index.js
+++ index.js
@@ -30,6 +30,18 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // database collections
+    const jobsCollections = client.db("jobPortal").collection("jobs");
+
+    app.get("/jobs", async (req, res) => {
+      const cursor = jobsCollections.find();
+      const result = await cursor.toArray();
+
+      res.send(result);
+    });
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
